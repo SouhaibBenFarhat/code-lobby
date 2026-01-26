f9b7903d5f29d57a746ab9465ec52c78fdf0cf94
Souhaib Ben Farhat <souhaib.b.farhat@gmail.com>
2026-01-26 17:52:04 +0100

    fix: add z-20 to Explorer resize handle to render above header

--- --module-explorer/components/IDEView.tsx
+++ --module-explorer/components/IDEView.tsx
@@ -457,7 +457,7 @@ export function IDEView({ currentUser }: IDEViewProps): React.JSX.Element {
         aria-valuenow={explorerWidth}
         tabIndex={0}
         className={cn(
-          'absolute right-0 top-0 bottom-0 w-1 cursor-col-resize hover:bg-primary/50 transition-colors',
+          'absolute right-0 top-0 bottom-0 w-1 z-20 cursor-col-resize hover:bg-primary/50 transition-colors',
           isResizing && 'bg-primary'
         )}
         onMouseDown={handleResizeStart}
